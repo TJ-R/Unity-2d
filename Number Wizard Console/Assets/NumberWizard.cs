23c1166c57b15d65b2db71f9d045585bd286ad28
Thomas Runyan <thomasrunyan13@gmail.com>
2020-06-16 12:23:24 -0400

    Finished simple game number wizard familarizing myself with C# syntax.

---
+++ Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int max;
+    int min;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to number wizard!");
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+        Debug.Log("Pick a number " + min + " through " + max);
+        Debug.Log("Tell me if your number is higher or lower than " + guess);
+        Debug.Log("Push Up = higher, Push Down = lower, Push Enter = Correct");
+        max = max + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Awesome, I found your number!");
+            StartGame();
+        }
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        Debug.Log("Is it higher or lower than: " + guess);
+    }
+}
