dde5aaff9491421732c431083d47f21b29ec8270
Thomas Runyan <thomasrunyan13@gmail.com>
2020-06-18 13:08:49 -0400

    Created a State Scriptable Object and set up starting state.

--- Text101/Assets/AdventureGame.cs
+++ Text101/Assets/AdventureGame.cs
@@ -6,11 +6,15 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour
 {
     [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State currentState;
 
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = ("Hello World Programmatically!!!");
+        currentState = startingState;
+        textComponent.text = currentState.GetStateStory();
     }
 
     // Update is called once per frame
