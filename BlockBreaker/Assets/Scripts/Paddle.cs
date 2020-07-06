9c6c6bff084604793e4ecc253e246904f94e7482
Thomas Runyan <thomasrunyan13@gmail.com>
2020-07-06 19:56:25 -0400

    Created Scenes and started working on logic for paddle and lose collider

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Configuration Parameters
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float minScreenPos = 1f;
+    [SerializeField] float maxScreenPos = 15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        float mouseXPosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mouseXPosInUnits, minScreenPos, maxScreenPos);
+        transform.position = paddlePos;
+    }
+}
