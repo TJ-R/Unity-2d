160e635bfe555b85bfc7b23405ee046c9538ff76
Thomas Runyan <thomasrunyan13@gmail.com>
2020-07-13 12:22:09 -0400

    Added audio, score, and scene loading loop.

--- BlockBreaker/Assets/Scripts/Block.cs
+++ BlockBreaker/Assets/Scripts/Block.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    // config params
+    [SerializeField] AudioClip breakAudio;
+
+    // cached reference
+    Level level;
+    GameSession gameStatus;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        gameStatus = FindObjectOfType<GameSession>();
+        level.CountBreakbleBlocks();
+
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject, 0.1f);
+        DestroyBlock();
+        gameStatus.addToScore();
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakAudio, Camera.main.transform.position, .3f);
+        Destroy(gameObject);
+        level.BlockDestroyed();
     }
 }
---
+++ BlockBreaker/Assets/Scripts/Level.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int breakableBlocks = 0; // Serialized for debugging purposes
+
+    // Cached Reference
+    SceneLoader sceneLoader;
+
+    public void CountBreakbleBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
