25e647ae01ac40531d30ebfc601f4c8a0bcfd471
Thomas Runyan <thomasrunyan13@gmail.com>
2020-07-20 11:35:10 -0400

    Made enemies shoot, both player and enemys can die, scrolling background, particle starfiled, particle death vfx and imported some sfx.

--- Laser Defender/Assets/Script/Enemy.cs
+++ Laser Defender/Assets/Script/Enemy.cs
@@ -1,25 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Enemy")]
     [SerializeField] float health = 100f;
+    [SerializeField] float shotCounter;
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOfDeath = .1f;
+
+    [Header("Projectile")]
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] float projectileSpeed = 4f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject enemyLaser = Instantiate(
+            laserPrefab,
+            transform.position,
+            Quaternion.identity) as GameObject;
+
+        enemyLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) { return; }
         ProcessHit(damageDealer);
     }
 
@@ -30,7 +63,17 @@ public class Enemy : MonoBehaviour
 
         if (health == 0)
         {
-            Destroy(gameObject);
+            Die();
         }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        if (!deathVFX) { return; }
+        GameObject explosion = Instantiate(deathVFX, 
+            transform.position, 
+            Quaternion.identity) as GameObject;
+        Destroy(explosion, durationOfDeath);
     }
 }
--- Laser Defender/Assets/Script/Player.cs
+++ Laser Defender/Assets/Script/Player.cs
@@ -8,8 +8,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // configuration parameters
+    [Header("Player")]
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float padding = 0.5f;
+    [SerializeField] int health = 200;
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOfDeath = .1f;
+
+    [Header("Projectile")]
     [SerializeField] GameObject laserPrefab;
     [SerializeField] float projectileSpeed = 15f;
     [SerializeField] float projectileFiringPeriod = 0.1f;
@@ -78,5 +84,33 @@ public class Player : MonoBehaviour
         xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
         yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
         yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) { return; }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+        if (!deathVFX) { return; }
+        GameObject explosion = Instantiate(deathVFX,
+            transform.position,
+            Quaternion.identity) as GameObject;
+        Destroy(explosion, durationOfDeath);
     }
 }
