4fa09fe73789d0478ff6cb20fb0d7c6d39be1714
Thomas Runyan <thomasrunyan13@gmail.com>
2020-07-24 11:21:38 -0400

    Created scripts for spawning attackers, defenders, can destroy attackers, animations for lizard and cactus, and created a UI to select which defenders want to placed in the game area

---
+++ Glitch Garden/Assets/Scripts/Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+
+    [Header("FX")]
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float durationOfDeath;
+
+    public void DealDamage(int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+
+        if (!deathVFX) { return; }
+        GameObject explosion = Instantiate(deathVFX, 
+            transform.position, 
+            Quaternion.identity);
+        Destroy(explosion, durationOfDeath);
+    }
+}
---
+++ Glitch Garden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] float rotationSpeed = 10f;
+    [SerializeField] int damage = 50;
+   void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        var health = other.GetComponent<Health>();
+        var attacker = other.GetComponent<Attacker>();
+
+        if (attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
